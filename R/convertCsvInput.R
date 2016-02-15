f3014912a9e131f6fd94287cf2247d5325c88994
masalmon <maelle.salmon@yahoo.se>
2016-02-15 15:45:15 +0100

    better comments of the code for converting input

--- R/convertCsvInput.R
+++ R/convertCsvInput.R
@@ -27,16 +27,20 @@
 #' @export
 convertInput <- function(pathResults, sepResults, quoteSign = "\'",
                          pathDicoCoding, sepDicoCoding, formatDate = "ymd") {
-
+    ########################################################
     # Get dico coding
+    ########################################################
     dicoCoding <- read.csv(pathDicoCoding, sep = sepDicoCoding, header = TRUE)
     dicoCoding$Code <- tolower(dicoCoding$Code)
     dicoCoding$Meaning <- tolower(dicoCoding$Meaning)
     dicoCoding$Group <- tolower(dicoCoding$Group)
 
+    ########################################################
     # open results
+    ########################################################
     resultsCoding <- read.table(pathResults, sep = sepResults,
                                 header = TRUE, quote = quoteSign)
+    # deal with different formats
     if(ncol(resultsCoding) != 4){
       resultsCoding <- resultsCoding[,1:3]
     }
@@ -47,29 +51,45 @@ convertInput <- function(pathResults, sepResults, quoteSign = "\'",
                                 "image_time",
                                 "annotation")
     }
+    ########################################################
+    # participantID
+    ########################################################
     # well participantID will not always make sense
     participantID <- as.character(resultsCoding[1, 1])
     imagePath <- unique(as.character(resultsCoding$"image_path"))
 
+    ########################################################
     # convert time date
+    ########################################################
     formatDate <- match.arg(formatDate, c("ymd", "mdy"))
     if (formatDate == "ymd") {
         timeDate <- lubridate::ymd_hms(
-          as.character(resultsCoding$"image_time"))[
-          !duplicated(as.character(resultsCoding$"image_path"))]
+          as.character(resultsCoding$"image_time"))
     }
     if (formatDate == "mdy") {
         timeDate <- lubridate::mdy_hms(
-          as.character(resultsCoding$"image_time"))[
-          !duplicated(as.character(resultsCoding$"image_path"))]
+          as.character(resultsCoding$"image_time"))
     }
+
+    ########################################################
     # Find all codes
+    ########################################################
+    # create empty vector
     codeResults <- rep("", length(imagePath))
+    # loop over pictures
+    # quite complicated
+    # because sometimes >1 line per picture
     for (i in 1:length(imagePath)) {
         codeResults[i] <- tolower(
           toString(as.character(resultsCoding$annotation[
           as.character(resultsCoding$"image_path") == imagePath[i]])))
     }
+
+    ########################################################
+    # then first I create the table with binary variables
+    ########################################################
+    # one column for each possible code,
+    # one line for each picture
     temp <- NULL
     namesTemp <- NULL
     for (code in dicoCoding$Code) {
@@ -79,6 +99,16 @@ convertInput <- function(pathResults, sepResults, quoteSign = "\'",
     temp <- as.data.frame(temp)
     names(temp) <- namesTemp
     codesBinaryVariables <- temp
+
+    ########################################################
+    # and now I create a list
+    ########################################################
+    # one value for each picture
+    # they are all codes for the same picture
+    # but separated by commas
+    # while in the original codeResults we could have
+    # other separators... and other code:
+    # here we only see what is in the dicoCoding.
     for (j in 1:ncol(temp)) {
         for (i in 1:nrow(temp)) {
             if (temp[i, j]) {
@@ -92,12 +122,16 @@ convertInput <- function(pathResults, sepResults, quoteSign = "\'",
     for (i in 1:nrow(temp)) {
         codes[i] <- toString(temp[i, ])
     }
+    ########################################################
+    # Done!
+    ########################################################
     wearableCamImagesObject <- new("wearableCamImages",
                                    dicoCoding = dicoCoding,
                                    imagePath = imagePath,
                                    timeDate = timeDate,
                                    codes = codes,
-                                   codesBinaryVariables = codesBinaryVariables,
+                                   codesBinaryVariables =
+                                     codesBinaryVariables,
                                    participantID = participantID)
     return(wearableCamImagesObject)
 }
